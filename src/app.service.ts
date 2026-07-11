17baca56ba9bbab98806189093efa6f41de6a395
Jean Carlos Lerma <yancalerma@gmail.com>
2026-07-11 12:16:04 -0500

    feat: modify message of api

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): { message: string; version: string; timestamp: string } {
+    return {
+      message: 'Welcome to the Inventory Management System API!',
+      version: '1.0.0',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
